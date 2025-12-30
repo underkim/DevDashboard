b09a16dc8c5b7a8d789737c3e80fc7ecd0d99e89
kim <rlaehdgns4568@gmail.com>
2025-12-30 22:26:53 +0900

    refactor: Entity Framework 규칙에 의해 자동 설정되는 설정 제거

--- DevDashboard.Data/AppDbContext.cs
+++ DevDashboard.Data/AppDbContext.cs
@@ -24,54 +24,10 @@ namespace DevDashboard.Data
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            // Problem
-            modelBuilder.Entity<Problem>()
-                .HasKey(p => p.Id);
 
-            modelBuilder.Entity<Problem>()
-                .Property(p => p.Source)
-                .IsRequired()
-                .HasMaxLength(20);
-
-            modelBuilder.Entity<Problem>()
-                .Property(p => p.ProblemNo)
-                .IsRequired()
-                .HasMaxLength(30);
-
-            modelBuilder.Entity<Problem>()
-                .Property(p => p.Title)
-                .IsRequired()
-                .HasMaxLength(200);
-
-            // ProblemTag 복합키
+            // 복합키만 설정
             modelBuilder.Entity<ProblemTag>()
                 .HasKey(pt => new { pt.ProblemId, pt.TagId });
-
-            modelBuilder.Entity<ProblemTag>()
-                .HasRequired(pt => pt.Problem)
-                .WithMany(p => p.ProblemTags)
-                .HasForeignKey(pt => pt.ProblemId);
-
-            modelBuilder.Entity<ProblemTag>()
-                .HasRequired(pt => pt.Tag)
-                .WithMany(t => t.ProblemTags)
-                .HasForeignKey(pt => pt.TagId);
-
-            // Solution
-            modelBuilder.Entity<Solution>()
-                .HasRequired(s => s.Problem)
-                .WithMany(p => p.Solutions)
-                .HasForeignKey(s => s.ProblemId);
-
-            // Tag
-            modelBuilder.Entity<Tag>()
-                .HasIndex(t => t.Name)
-                .IsUnique();
-
-            // DailyStats
-            modelBuilder.Entity<DailyStats>()
-                .HasIndex(d => d.Date)
-                .IsUnique();
 
             base.OnModelCreating(modelBuilder);
         }
