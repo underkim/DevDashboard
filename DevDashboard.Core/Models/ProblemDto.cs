227e874f82d5fddb992ef53ae58a5a5879ececf2
UnderKim <rlaehdgns4568@gmail.com>
2026-01-02 15:48:44 +0900

    feat: ProblemDto, SolutionDto 추가

---
+++ DevDashboard.Core/Models/ProblemDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevDashboard.Core.Models
+{
+    public class ProblemDto
+    {
+        public ProblemDto()
+        {
+            Tags = new List<string>();
+        }
+
+        public int Id { get; set; }
+        public string Source { get; set; }
+        public string ProblemNo { get; set; }
+        public string Title { get; set; }
+        public string Difficulty { get; set; }
+        public string Url { get; set; }
+        public double? AcceptanceRate { get; set; }
+        public List<string> Tags { get; set; }
+        public int SolutionCount { get; set; }
+    }
+}
---
+++ DevDashboard.Core/Models/SolutionDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevDashboard.Core.Models
+{
+    public class SolutionDto
+    {
+        public int Id { get; set; }
+        public int ProblemId { get; set; }
+        public string ProblemTitle { get; set; }
+        public string Code { get; set; }
+        public string Language { get; set; }
+        public int? ExecutionTime { get; set; }
+        public int? MemoryUsage { get; set; }
+        public string Result { get; set; }
+        public int? TimeSpentMinutes { get; set; }
+        public string Memo { get; set; }
+        public DateTime SubmittedAt { get; set; }
+    }
+}
